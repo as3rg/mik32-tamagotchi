878fd0777e2dd07c360e69ae12eced5cb6839da6
Michael Latushko <m.latushko@yadro.com>
2025-04-01 05:00:14 +0000

    display driver added:

--- app/main.c
+++ app/main.c
@@ -1,6 +1,7 @@
 #include "mik32_hal_usart.h"
 #include "mik32_hal_scr1_timer.h"
-
+#include "mik32_hal_i2c.h"
+#include "lcd_driver.h"
 /*
  * Данный пример демонстрирует вывод данных через USART.
  */
@@ -8,27 +9,40 @@
 
 static void SystemClock_Config();
 static void USART_Init();
-static uint32_t fibonachi(uint32_t n);
+static void I2C_Init();
 
 static USART_HandleTypeDef husart0;
+I2C_HandleTypeDef hi2c;
 
 int main()
 {
-    // volatile только чтобы оставить unused переменные для отладки
-    volatile uint32_t fibonachi_result;
-    volatile uint64_t start_mtime, end_mtime;
     SystemClock_Config();
-
     USART_Init();
+    I2C_Init();    
+    
+    uint8_t data[1] = { 0x55 };
+    uint8_t size = 8; 
+
+
+    lcd_init ();
+    lcd_put_cur(0, 0);
+    lcd_send_string ("HELLO WORLD");
+    lcd_put_cur(1, 0);
+    lcd_send_string("from CTECH");
 
     while (1)
     {
-        HAL_USART_Print(&husart0, "YADRO RISCV\r\n", USART_TIMEOUT_DEFAULT);
-        start_mtime = __HAL_SCR1_TIMER_GET_TIME();
-        fibonachi_result = fibonachi(30);
-        end_mtime = __HAL_SCR1_TIMER_GET_TIME();
-        // здесь можно вывести время выполнения функции
-        HAL_DelayMs(5000);
+        if(HAL_OK == HAL_I2C_Master_Transmit(&hi2c, 0x27, data, size, I2C_TIMEOUT_DEFAULT)) {
+            HAL_USART_Print(&husart0, "I2C Example Start\r\n", USART_TIMEOUT_DEFAULT);
+        }
+
+
+        lcd_put_cur(0, 2);
+        lcd_send_string ("Hello, Yadro");
+   
+      
+        
+        HAL_DelayMs(1000);
     }
 }
 
@@ -89,19 +103,28 @@ void USART_Init()
     husart0.Modem.dsr = Disable; //in
     husart0.Modem.ri = Disable;  //in
     husart0.Modem.ddis = Disable;//out
-    husart0.baudrate = 9600;
+    husart0.baudrate = 115200;
     HAL_USART_Init(&husart0);
 }
 
-uint32_t fibonachi(uint32_t n)
-{
-    if (n == 0)
+void I2C_Init() {
+    hi2c.Instance = I2C_1;
+    hi2c.Init.Mode = HAL_I2C_MODE_MASTER;
+
+    hi2c.Init.DigitalFilter = I2C_DIGITALFILTER_2CLOCKCYCLES;
+    hi2c.Init.AnalogFilter = I2C_ANALOGFILTER_DISABLE;
+    hi2c.Init.AutoEnd = I2C_AUTOEND_ENABLE;
+
+    /* Настройка частоты */
+    hi2c.Clock.PRESC = 4;
+    hi2c.Clock.SCLDEL = 7;
+    hi2c.Clock.SDADEL = 7;
+    hi2c.Clock.SCLH = 31;
+    hi2c.Clock.SCLL = 31;
+
+    if (HAL_I2C_Init(&hi2c) != HAL_OK)
     {
-        return 0;
+        HAL_USART_Print(&husart0, "I2C init error\r\n", USART_TIMEOUT_DEFAULT);
     }
-    if ((n == 1) || (n == 2))
-    {
-        return 1;
-    }
-    return fibonachi(n - 1) + fibonachi(n - 2);
 }
+
